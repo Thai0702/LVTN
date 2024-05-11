4690db9a8a0f4db257a6d9b3552bfe684b0d2175
Thai0702 <levanthai20072002@gmail.com>
2024-05-11 10:41:20 +0700

    LVTN

---
+++ src/main/java/com/lvtn/demo/demo.java
@@ -0,0 +1,15 @@
+package com.lvtn.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+
+    @GetMapping ("/welcome")
+    public String welcome()
+    {
+        return "hello world";
+    }
+
+}
